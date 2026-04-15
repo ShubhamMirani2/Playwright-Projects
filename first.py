e8f790102c2ced113265df6902192377cb42457a
Shubham <shubham.v.mirani@gmail.com>
2026-04-15 16:34:40 +0530

    slow change commit

--- first.py
+++ first.py
@@ -2,14 +2,14 @@ from playwright.sync_api import sync_playwright
 
 with sync_playwright() as playwright:
 
-    browser = playwright.chromium.launch(headless=False,slow_mo=2000) #lanuch Browser
+    browser = playwright.chromium.launch(headless=False,slow_mo=4000) #lanuch Browser
     page = browser.new_page() #Create new page 
     page.goto("https://playwright.dev/python/")
 
-    docs_button = page.get_by_role('link', name="Docs")
+    docs_button = page.get_by_role('button', name="log Out")
     docs_button.click()
 
-    print("Docs:", page.url)
+    
 
     browser.close()
 
